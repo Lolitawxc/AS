c69693a16656783a787e4d003bb7c145dc38ddf4
KaaZMi <mikael.14523@gmail.com>
2017-01-06 19:31:55 +0100

    Introduction de la masked convolution au squelette

--- usps_pixelrnn.lua
+++ usps_pixelrnn.lua
@@ -10,34 +10,34 @@ maxIterations = 100000
 
 function create_pixelrnn()
   model = "pixel_rnn" -- name of model [pixel_rnn, pixel_cnn]
-  hidden_dims = 16 -- dimesion of hidden states of LSTM or Conv layers
+  hidden_dims = 16 -- dimension of hidden states of LSTM or Conv layers
   recurrent_length = 7 -- the length of LSTM or Conv layers
   out_hidden_dims = 32 -- dimesion of hidden states of output Conv layers
   out_recurrent_length = 2 -- the length of output Conv layers
   data = "usps" -- name of dataset [usps, mnist, cifar]
   height, width, channel = 16,16,1
 
-  print("Building starts!" .. model)
+  print("Building starts -> " .. model)
   local prnn = nn.Sequential()
 
 
   -- input of main reccurent layers : 7x7 conv, mask A
   print("Building input layer")
-  prnn:add( conv2d(hidden_dims * 2, {7, 7}, "A") ) -- TODO
+  prnn:add( nn.MaskedConvolution(channel, hidden_dims * 2, 7, 7, "A") )
 
   
-  -- main reccurent layers : DiagonalBiLSTM layers
-  for idx=1,recurrent_length do
-    print("Building DiagonalBiLSTM layer " .. idx)
-    prnn:add( diagonal_bilstm() ) -- TODO
-  end
+  -- -- main reccurent layers : DiagonalBiLSTM layers
+  -- for idx=1,recurrent_length do
+  --   print("Building DiagonalBiLSTM layer " .. idx)
+  --   prnn:add( diagonal_bilstm() ) -- TODO
+  -- end
 
 
   -- output reccurent layers : ReLU followed by 1x1 conv, mask B (2 layers)
   for idx=1,out_recurrent_length do
     print("Building output layer" .. idx)
     prnn:add( nn.ReLU() )
-    prnn:add( conv2d(out_hidden_dims, {1, 1}, "B") ) -- TODO
+    prnn:add( nn.MaskedConvolution(channel, out_hidden_dims, 1, 1, "B") )
   end
 
 
@@ -45,10 +45,48 @@ function create_pixelrnn()
   print("Building Sigmoid")
   prnn:add( nn.Sigmoid() )
 
-  print("Building finished!" .. model)
+  print("Building finished -> " .. model)
+
+  return prnn
 
 end
 
+
+---------------------------
+--- CUSTOM CONVOLUTION ----
+---------------------------
+local MaskedConvolution, parent = torch.class('nn.MaskedConvolution', 'nn.SpatialConvolution')
+
+-- see : https://github.com/torch/nn/blob/master/doc/convolution.md
+
+-- nInputPlane: The number of expected input planes in the image given into forward().
+-- nOutputPlane: The number of output planes the convolution layer will produce.
+-- kW: The kernel width of the convolution
+-- kH: The kernel height of the convolution
+-- mask : type of mask [A,B,None]
+function MaskedConvolution:__init(nInputPlane, nOutputPlane, kW, kH, mask, dW, dH, padW, padH)
+  parent.__init(self, nInputPlane, nOutputPlane, kW, kH, dW, dH, padW, padH)
+  self.mask = mask
+  self:applyMask()
+end
+
+-- Apply the mask, if there is one, to the weights.
+-- No mask means traditional spatial convolution then nothing more is done to the weights.
+function MaskedConvolution:applyMask()
+  if self.mask == "A" then
+    -- TODO
+  elseif self.mask == "B" then
+    -- TODO
+  end
+end
+
+
+----------------------
+--- BASELINE CODE ----
+----------------------
+--------- || ---------
+--------- || ---------
+--------- \/ ---------
 
 -- here we set up the architecture of the neural network
 function create_network(nb_outputs)
@@ -124,11 +162,15 @@ end
 function main()
 
   local training_dataset, testing_dataset, classes, classes_names = dofile('usps_dataset.lua')
-  local network = create_network(#classes)
+  -- local network = create_network(#classes)
+  local prnn = create_pixelrnn()
+  print(prnn)
 
-  print(testing_dataset[1][1])
-  -- gnuplot.imagesc(testing_dataset[1][1]:reshape(16,16))
-  image.display{image=testing_dataset[1][1]:reshape(16,16), zoom=20}
+
+  -- print(testing_dataset[1][1])
+  -- -- gnuplot.imagesc(testing_dataset[1][1]:reshape(16,16))
+  -- image.display{image=testing_dataset[1][1]:reshape(16,16), zoom=20}
+
 
   -- train_network(network, training_dataset)
   -- test_predictor(network, testing_dataset, classes, classes_names)
