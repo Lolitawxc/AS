5c75b606b5cbdfb3ef6d662606aba6a8ce4bab65
KaaZMi <mikael.14523@gmail.com>
2017-01-28 17:27:13 +0100

    Erreur commit precedent

--- MaskedConvolution.lua
+++ MaskedConvolution.lua
@@ -1,84 +1,67 @@
 -- see : https://github.com/torch/nn/blob/master/doc/convolution.md
-
 local MaskedConvolution, Parent = torch.class('nn.MaskedConvolution', 'nn.Module')
 
-function MaskedConvolution:__init(data_type, mask_type,
-                                 nInputPlane, nOutputPlane, kW, kH, dW, dH, padW, padH,
-                                 verbose)
-   -- data_type: `cudnn` or `nn`
-   -- mask_type: `A` or 'B' or nil (none)
-   -- this module is masked before weight updates,
-   -- technically, we apply a mask to `gradOutput` before `accGradParameters()`
+function MaskedConvolution:__init(mask_type, nInputPlane, nOutputPlane, kW, kH, dW, dH, padW, padH)
+  -- mask_type: 'A' or 'B' or nil (no mask)
+  -- This module is masked before weight updates. We apply a mask to 'gradOutput' before 'accGradParameters()'.
 
-   Parent.__init(self)
+  Parent.__init(self)
 
-   if mask_type then
-      self.m = torch.Tensor(kH, kW):fill(1)
-      if ((kH + 1) / 2 < kH) then
-         self.m[{{(kH + 1)/2, kH}, {(kW + 1)/2 + 1, kW}}] = 0
-         self.m[{{(kH + 1)/2 + 1, kH}, {1, kW}}] = 0
-      end
-      if mask_type == 'A' then
-         self.m[(kH + 1)/2][(kW + 1)/2] = 0
-      end
-   end
+  self.mask = torch.Tensor(nOutputPlane, nInputPlane, kH, kW):fill(1)
 
-   if self.verbose then
-      print('mask', self.m)
-   end
+  if mask_type then
+    center_h = (kH + 1)/2
+    center_w = (kW + 1)/2
 
-   self.mask = torch.Tensor(nOutputPlane, nInputPlane, kH, kW) -- not use forloop in lua to avoid slowdown
-   for i = 1, nOutputPlane do
-      for j = 1, nInputPlane do
-         self.mask[i][j] = self.m
-      end
-   end
+    if ((kH + 1) / 2 < kH) then
+      self.mask[{ {}, {}, { center_h  , kH }, { center_w+1, kW } }] = 0
+      self.mask[{ {}, {}, { center_h+1, kH }, { 1         , kW } }] = 0
+    end
 
-   if data_type == 'cudnn' then
-   else
-      self.conv = nn.SpatialConvolution(nInputPlane, nOutputPlane, kW, kH, dW, dH, padW, padH)
-   end
+    if mask_type == 'A' then
+      self.mask[{ {}, {}, center_h, center_w }] = 0
+    end
 
-   local w, gw = self.conv:parameters()
-   self.weight = w[1]
-   self.bias = w[2]
-   self.kH = kH
-   self.kW = kW
-   self.nInputPlane = nInputPlane
-   self.nOutputPlane = nOutputPlane
-   self.verbose = verbose
+  end
+
+  -- print(self.mask[{ 1, 1, {}, {} }])
+
+  -- For the rest it's an usual SpatialConvolution
+  self.conv = nn.SpatialConvolution(nInputPlane, nOutputPlane, kW, kH, dW, dH, padW, padH)
+
+  local w, gw = self.conv:parameters()
+  self.weight = w[1]
+  self.bias = w[2]
+  self.kH = kH
+  self.kW = kW
+  self.nInputPlane = nInputPlane
+  self.nOutputPlane = nOutputPlane
 
 end
 
 function MaskedConvolution:updateOutput(input)
-   self:clearWeight()
-   self.output = self.conv:forward(input)
-   return self.output
+  self:applyMask()
+  self.output = self.conv:forward(input)
+  return self.output
 end
 
 function MaskedConvolution:updateGradInput(input, gradOutput)
-   self.gradInput = self.conv:updateGradInput(input, gradOutput)
-   return self.gradInput
+  self.gradInput = self.conv:updateGradInput(input, gradOutput)
+  return self.gradInput
 end
 
 function MaskedConvolution:accGradParameters(input, gradOutput, scale)
-   self.conv:accGradParameters(input, gradOutput, scale)
+  self.conv:accGradParameters(input, gradOutput, scale)
 end
 
-function MaskedConvolution:clearWeight()
-   if self.verbose then
-      print("clear weight of Mask Conv!")
-   end
-   self.weight:cmul(self.mask)
-end
-
-function MaskedConvolution:reset()
+function MaskedConvolution:applyMask()
+  self.weight:cmul(self.mask)
 end
 
 function MaskedConvolution:zeroGradParameters()
-   self.conv:zeroGradParameters()
+  self.conv:zeroGradParameters()
 end
 
 function MaskedConvolution:updateParameters(learningRate)
-   self.conv:updateParameters(learningRate)
+  self.conv:updateParameters(learningRate)
 end
--- mnist_pixelcnn.lua
+++ mnist_pixelcnn.lua
@@ -1,96 +1,11 @@
-require "torch"
-require "nn"
-require "math"
-require "gnuplot"
-require "image"
-
-----------------------------------
-------- MASKED CONVOLUTION -------
-----------------------------------
-
--- see : https://github.com/torch/nn/blob/master/doc/convolution.md
-local MaskedConvolution, Parent = torch.class('nn.MaskedConvolution', 'nn.Module')
-
-function MaskedConvolution:__init(data_type, mask_type,
-                                 nInputPlane, nOutputPlane, kW, kH, dW, dH, padW, padH,
-                                 verbose)
-   -- data_type: 'cudnn' or 'nn'
-   -- mask_type: 'A' or 'B' or nil (none)
-   -- this module is masked before weight updates,
-   -- technically, we apply a mask to 'gradOutput' before 'accGradParameters()'
-
-   Parent.__init(self)
-
-   if mask_type then
-      self.m = torch.Tensor(kH, kW):fill(1)
-      if ((kH + 1) / 2 < kH) then
-         self.m[{{(kH + 1)/2, kH}, {(kW + 1)/2 + 1, kW}}] = 0
-         self.m[{{(kH + 1)/2 + 1, kH}, {1, kW}}] = 0
-      end
-      if mask_type == 'A' then
-         self.m[(kH + 1)/2][(kW + 1)/2] = 0
-      end
-   end
-
-   if self.verbose then
-      print('mask', self.m)
-   end
-
-   self.mask = torch.Tensor(nOutputPlane, nInputPlane, kH, kW) -- not use forloop in lua to avoid slowdown
-   for i = 1, nOutputPlane do
-      for j = 1, nInputPlane do
-         self.mask[i][j] = self.m
-      end
-   end
-
-   if data_type == 'cudnn' then
-   else
-      self.conv = nn.SpatialConvolution(nInputPlane, nOutputPlane, kW, kH, dW, dH, padW, padH)
-   end
-
-   local w, gw = self.conv:parameters()
-   self.weight = w[1]
-   self.bias = w[2]
-   self.kH = kH
-   self.kW = kW
-   self.nInputPlane = nInputPlane
-   self.nOutputPlane = nOutputPlane
-   self.verbose = verbose
-
-end
-
-function MaskedConvolution:updateOutput(input)
-   self:clearWeight()
-   self.output = self.conv:forward(input)
-   return self.output
-end
-
-function MaskedConvolution:updateGradInput(input, gradOutput)
-   self.gradInput = self.conv:updateGradInput(input, gradOutput)
-   return self.gradInput
-end
-
-function MaskedConvolution:accGradParameters(input, gradOutput, scale)
-   self.conv:accGradParameters(input, gradOutput, scale)
-end
-
-function MaskedConvolution:clearWeight()
-   if self.verbose then
-      print("clear weight of Mask Conv!")
-   end
-   self.weight:cmul(self.mask)
-end
-
-function MaskedConvolution:reset()
-end
-
-function MaskedConvolution:zeroGradParameters()
-   self.conv:zeroGradParameters()
-end
-
-function MaskedConvolution:updateParameters(learningRate)
-   self.conv:updateParameters(learningRate)
-end
+require 'torch'
+require 'nn'
+require 'math'
+require 'gnuplot'
+require 'image'
+require 'rnn'
+require 'MaskedConvolution'
+require 'RowLSTM'
 
 ---------------------------------
 ------------ NETWORK ------------
@@ -100,7 +15,7 @@ end
 local function create_pixelcnn(opt)
   -- opt: model parameters in dictionary
 
-  local function testModel(model)
+  local function check(model)
     local imageSize = 28
     local input = torch.randn(1,1,imageSize,imageSize):type(model._type)
     print('input', input)
@@ -113,18 +28,20 @@ local function create_pixelcnn(opt)
   local dim = 16
   local model = nn.Sequential()
   model:add(nn.Reshape(1, 28, 28))
-  model:add(nn.MaskedConvolution('nn', 'A', 1, dim, 7, 7, 1, 1, 3, 3)) -- 7x7 conv, mask A
+  model:add(nn.MaskedConvolution('A', 1, dim, 7, 7, 1, 1, 3, 3)) -- 7x7 conv, mask A
   
   if opt.model == 'cnn' then
-    model:add(nn.MaskedConvolution('nn', 'B', dim, dim, 3, 3, 1, 1, 1, 1)) -- 3x3 conv, mask B (can be more than one layer)
+    model:add(nn.MaskedConvolution('B', dim, dim, 3, 3, 1, 1, 1, 1)) -- 3x3 conv, mask B (can be more than one layer)
   elseif opt.model == 'row' then
-    -- model:add(nn.RowLSTM()) -- TODO
-    model:add(nn.MaskedConvolution('nn', 'B', dim, dim, 3, 1, 1, 1, 1, 1)) -- 3x1 conv, mask B
-    model:add(nn.MaskedConvolution('nn', nil, dim, dim, 3, 1, 1, 1, 1, 1)) -- 3x1 conv, no mask
+    model:add(nn.RowLSTM(dim))
+    -- We have to add masked convolutions below into our RowLSTM module.
+    -- model:add(nn.MaskedConvolution('B', dim, dim, 3, 1, 1, 1, 1, 1)) -- Input-State : 3x1 conv, mask B
+    -- model:add(nn.MaskedConvolution(nil, dim, dim, 3, 1, 1, 1, 1, 1)) -- State-State : 3x1 conv, no mask
   elseif opt.model == 'diag' then
     -- model:add(nn.DiagonalBiLSTM()) -- TODO
-    model:add(nn.MaskedConvolution('nn', 'B', dim, dim, 1, 1, 1, 1, 1, 1)) -- 1x1 conv, mask B
-    model:add(nn.MaskedConvolution('nn', nil, dim, dim, 1, 2, 1, 1, 1, 1)) -- 1x2 conv, no mask
+    -- We have to add masked convolutions below into our DiagonalBiLSTM module.
+    -- model:add(nn.MaskedConvolution('B', dim, dim, 1, 1, 1, 1, 1, 1)) -- Input-State : 1x1 conv, mask B
+    -- model:add(nn.MaskedConvolution(nil, dim, dim, 1, 2, 1, 1, 1, 1)) -- State-State : 1x2 conv, no mask
   end
 
   model:add(nn.ReLU())
@@ -140,7 +57,7 @@ local function create_pixelcnn(opt)
     model:add(nn.Sigmoid())
   end
 
-  -- testModel(model)
+  -- check(model)
 
   -- init
   for k,v in pairs(model:findModules'nn.Linear') do
@@ -161,7 +78,7 @@ end
 -- train a neural network
 function fit(network, dataset, maxIterations, learningRate)
     
-  print("Training the network")
+  print('Training the network')
   local criterion = nn.BCECriterion() -- others to try : nn.MultiLabelSoftMarginCriterion(), nn.ClassNLLCriterion() ; or on CUDA : cudnn.VolumetricCrossEntropyCriterion()
 
   for iteration=1,maxIterations do
@@ -181,15 +98,17 @@ function fit(network, dataset, maxIterations, learningRate)
     network:backward(x, delta)
     network:updateParameters(learningRate)
   end
-  print("Network trained")
+  print('Network trained')
 
 end
 
---------------------------------
----------- GENERATION ----------
---------------------------------
+-------------------------------------
+------ GENERATION / COMPLETION ------
+-------------------------------------
 
 function generate(model)
+  -- Generates an image from an empty image.
+
   local imageSize = 28
   local sample = torch.Tensor(imageSize,imageSize):zero():type(model._type)
   --print('init input', sample)
@@ -203,7 +122,35 @@ function generate(model)
     end
   end
 
-  image.display{image=new_sample:reshape(imageSize,imageSize), zoom=20}
+  image.display{image=sample:reshape(imageSize,imageSize), zoom=20}
+end
+
+function complete(model, sample)
+  -- Complete an image from its first half.
+
+  local imageSize = 28
+  sample = torch.reshape(sample,imageSize,imageSize):type(model._type)
+
+  -- print('init input', sample)
+
+  -- Hide the bottom half of the image
+  for i=imageSize/2,imageSize do
+    sample[{ i, {} }] = 0
+  end
+
+  -- image.display{image=sample:reshape(imageSize,imageSize), zoom=20}
+
+  -- Try to find the hidden part
+  for i=imageSize/2,imageSize do
+    for j=1,imageSize do
+      new_sample = model:forward(sample):reshape(imageSize,imageSize)
+      -- print('output', new_sample)
+      sample[{ i, j }] = new_sample[{ i, j }]
+      -- print('result', sample)
+    end
+  end
+
+  image.display{image=sample:reshape(imageSize,imageSize), zoom=20}
 end
 
 --------------------------
@@ -216,7 +163,7 @@ cmd:option('-usegpu', false, 'use gpu for training')
 cmd:option('-crit', 'sigmoid', 'use sigmoid or softmax')
 cmd:option('-model', 'cnn', 'row: RowLSTM, diag:DiagonalBiLSTM, cnn: PixelCNN')
 local config = cmd:parse(arg)
-print(config)
+-- print(config)
 
 -- USPS dataset
 -- local training_dataset, testing_dataset, classes, classes_names = dofile('usps_dataset.lua')
@@ -226,13 +173,14 @@ print(config)
 local mnist = require 'mnist'
 local dataset = mnist['test' .. 'dataset']() -- test set for the moment (less memory cost)
 dataset.data = dataset.data:reshape(dataset.data:size(1), dataset.data:size(2) * dataset.data:size(3)):double()
-print(dataset.data:size(1))
-print(dataset.label[1])
+-- print(dataset.data:size(1))
+-- print(dataset.label[1])
 
 -- Network
 local network = create_pixelcnn(config)
 print(network)
 
 -- Train
-fit(network, dataset, 1000, 0.01)
+fit(network, dataset, 50, 0.01)
 generate(network)
+-- complete(network,dataset.data[5])
